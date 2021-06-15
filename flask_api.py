7977c182ffba42a4a02ed3466e8f97cd96e243d4
DIF21 <gerson.correia@aluno.faculdadeimpacta.com.br>
2021-06-14 23:58:04 -0300

    -a

--- flask_api.py
+++ flask_api.py
@@ -573,5 +573,5 @@ def editar(url, body):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
     #app.run(host='localhost', port=5000, debug=True)
